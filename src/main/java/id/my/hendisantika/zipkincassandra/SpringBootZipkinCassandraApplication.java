814371fb7a7a8adbf38875d0dc8c419ed9694906
hendisantika <hendisantika@yahoo.co.id>
2024-06-06 07:10:50 +0700

    Create Spring Boot Project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/zipkincassandra/SpringBootZipkinCassandraApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.zipkincassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootZipkinCassandraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootZipkinCassandraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/zipkincassandra/SpringBootZipkinCassandraApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.zipkincassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootZipkinCassandraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
