fe5c1cedb287033c2385e2f0a2bab7e310c86542
hendisantika <hendisantika@yahoo.co.id>
2024-06-06 07:15:19 +0700

    Add Book DTO🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/zipkincassandra/dto/BookDto.java
@@ -0,0 +1,26 @@
+package id.my.hendisantika.zipkincassandra.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-zipkin-cassandra
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/6/24
+ * Time: 07:15
+ * To change this template use File | Settings | File Templates.
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class BookDto {
+    private Long id;
+    private String bookName;
+    private String writer;
+}
